3472a07ca009ab8ade5d3efe7e5f95ddf7064423
subinbab <subinbabu4127@gmail.com>
2022-04-12 10:44:35 +0530

    LoginViewModel.cs  ADDED

---
+++ DomainLayer/Users/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainLayer.Users
+{
+    public class LoginViewModel
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+}
