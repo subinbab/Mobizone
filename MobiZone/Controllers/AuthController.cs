7bcf06a75e02d79b56bf2db45c18694e50aa2c00
subinbab <subinbabu4127@gmail.com>
2022-04-12 10:46:49 +0530

    AUTH CONTROLLER ADDED

---
+++ MobiZone/Controllers/AuthController.cs
@@ -0,0 +1,76 @@
+﻿using ApiLayer.Messages;
+using ApiLayer.Models;
+using AutoMapper;
+using BusinessObjectLayer.User;
+using DomainLayer.Users;
+using DTOLayer.UserModel;
+using log4net;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ApiLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILog _log;
+        ProductDbContext _userContext;
+        IUserCreate _userCreate;
+        UserRegistration _user;
+        IMessages _userMessages;
+        ResponseModel<UserDataViewModel> _userResponse;
+        IMapper _mapper;
+        IEnumerable<UserRegistration> _userList;
+        List<UserDataViewModel> _userDataList;
+        IWebHostEnvironment _webHostEnvironment;
+        Security _sec;
+        public AuthController(ProductDbContext userContext, IUserCreate userCreate, IMapper mapper, IWebHostEnvironment web)
+        {
+            _webHostEnvironment = web;
+            _userContext = userContext;
+            _userCreate = userCreate;
+            _user = new UserRegistration();
+            _userMessages = new UserMessages(_webHostEnvironment);
+            _userResponse = new ResponseModel<UserDataViewModel>();
+            _log = LogManager.GetLogger(typeof(UserController));
+            _mapper = mapper;
+            _userDataList = new List<UserDataViewModel>();
+            _sec = new Security();
+        }
+        [HttpPost]
+        public IActionResult post(LoginViewModel data)
+        {
+            try
+            {
+                ResponseModel<UserRegistration> _response = new ResponseModel<UserRegistration>();
+                string message;
+                string password = _sec.Encrypt("subin", data.password);
+                UserRegistration check = _userCreate.Authenticate(data.userName, password);
+                if (check != null)
+                {
+                    message = _userMessages.Added + new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+                    _response.AddResponse(true, 0, check, message);
+                    return new JsonResult(_response);
+                }
+                message = _userMessages.Null;
+                _response.AddResponse(false, 0, null, message);
+                return new JsonResult(_response);
+            }
+            catch (Exception ex)
+            {
+                ResponseModel<string> _response = new ResponseModel<string>();
+                _response.AddResponse(false, 0, null, ex.Message);
+                return new JsonResult(_response);
+            }
+
+        }
+    }
+}
