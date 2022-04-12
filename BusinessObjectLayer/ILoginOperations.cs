827258fd7634f552907384eb00b19ea6c2e51d92
subinbab <subinbabu4127@gmail.com>
2022-04-12 20:12:39 +0530

    loginOperations added

---
+++ BusinessObjectLayer/ILoginOperations.cs
@@ -0,0 +1,16 @@
+﻿using DomainLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessObjectLayer
+{
+    public interface ILoginOperations
+    {
+        void Add(Login entity);
+        void Edit(Login entity);
+        IEnumerable<Login> Get();
+        Login Get(int id);
+        void Delete(Login entity);
+    }
+}
---
+++ BusinessObjectLayer/LoginOperations.cs
@@ -0,0 +1,45 @@
+﻿using DomainLayer;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessObjectLayer
+{
+    public class LoginOperations : ILoginOperations
+    {
+        ProductDbContext _context;
+        IRepositoryOperations<Login> _repo;
+
+        public LoginOperations(IRepositoryOperations<Login> repo)
+        {
+
+            _repo = repo;
+        }
+        public void Add(Login entity)
+        {
+            _repo.Add(entity);
+            _repo.Save();
+        }
+
+        public void Delete(Login entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Login entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Login> Get()
+        {
+            return _repo.Get(n1 => n1.roles);
+        }
+
+        public Login Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
