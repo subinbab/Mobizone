61f1561a0ab721b8b987a18d4d5d8849d63a3ae9
Subin Babu <subabu@suyati.com>
2022-05-01 12:57:47 +0530

    added required annotation for some fields

--- DomainLayer/ProductModel/Specificatiion.cs
+++ DomainLayer/ProductModel/Specificatiion.cs
@@ -12,14 +12,19 @@ namespace DomainLayer.ProductModel
         public int id { get; set; }
         [NotMapped]
         public List<string> ram { get; set; }
-        public ICollection<Ram> rams { get; set; }
-        public ICollection<Storage> storages { get; set; }
+        public ICollection<Ram>? rams { get; set; }
+        public ICollection<Storage>? storages { get; set; }
         [NotMapped]
         public List<string> storage { get; set; }
+        [Required]
         public string simType { get; set; }
+        [Required]
         public string processor { get; set; }
+        [Required]
         public string core { get; set; }
+    
         public string os { get; set; }
+   
         public int? camFeatures { get; set; }
     }
 }
