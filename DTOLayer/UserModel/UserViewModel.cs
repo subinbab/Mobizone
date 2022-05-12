a63da42be0c36d0d8529f1501baa1a2731b711aa
Anjali v s <asreekala@suyati.com>
2022-05-12 18:47:07 +0530

    feedbacks edited

--- DTOLayer/UserModel/UserViewModel.cs
+++ DTOLayer/UserModel/UserViewModel.cs
@@ -9,28 +9,28 @@ namespace DTOLayer.UserModel
 {
   public  class UserViewModel  
     {
-        [Required (ErrorMessage = "This Field is Required")]
+        [Required (ErrorMessage = "This field is required")]
         [Display(Name = "First Name")]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$",ErrorMessage ="This field should not contain any numbers or special characters")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$",ErrorMessage = "Firstname must contain only letters")]
         public string FirstName { get; set; }
-        [Required(ErrorMessage = "This Field is Required")]
+        [Required(ErrorMessage = "This field is required")]
         [Display(Name = "Last Name")]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = "This field should not contain any numbers or special characters")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = "Lastname must contain only letters")]
         public string LastName { get; set; }
-        [Required(ErrorMessage = "This Field is Required")]
+        [Required(ErrorMessage = "This field is required")]
         [Display(Name = "Email")]
         [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z\s]+\.[a-zA-Z\s.]+$",ErrorMessage="Invalid Email format")]
         [EmailAddress(ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
-        [Required(ErrorMessage = "This Field is Required")]
+        [Required(ErrorMessage = "This field is required")]
         [Display(Name = "Password")]
         [DataType(DataType.Password)]
-        [StringLength(30, MinimumLength = 8, ErrorMessage = "Your password must be at least 8 characters long")]
+        [StringLength(30, MinimumLength = 8, ErrorMessage = " password must be  8 characters ")]
         public string Password { get; set; }
         [Display(Name = "Confirm Password")]
         [DataType(DataType.Password)]
         [Compare("Password")]
-        [StringLength(30, MinimumLength = 8, ErrorMessage = "Your password must be at least 8 characters long")]
+        [StringLength(30, MinimumLength = 8, ErrorMessage = "password must be  8 characters ")]
         public string ConfirmPassword { get; set; }
     }
 }
