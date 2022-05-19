b9c2e19d552f9349f4d6b0c272c85c6efce236ce
subinbab <subinbabu4127@gmail.com>
2022-05-19 14:49:49 +0530

    solved

--- DomainLayer/Cart.cs
+++ DomainLayer/Cart.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace DomainLayer
@@ -11,6 +12,7 @@ namespace DomainLayer
         public int id { get; set; }
         public string sessionId { get; set; }
         public ICollection<CartDetails> cartDetails { get; set; }
+        [ForeignKey("userId")]
         public UserRegistration? userId { get; set; }
     }
 }
