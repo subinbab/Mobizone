348fc86ded47c700716e6cab18c89dca8132ffba
Akza <avarghese@suyati.com>
2022-05-25 11:19:30 +0530

    changes done

--- DomainLayer/MyCart.cs
+++ DomainLayer/MyCart.cs
@@ -1,18 +1,16 @@
 ﻿using DomainLayer.Users;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace DomainLayer
 {
-    public class DbCart
+    public class MyCart
     {
-        [Column("Id")]
         public int id { get; set; }
         public string? sessionId { get; set; }
-        [ForeignKey("DbCartId")]
         public ICollection<CartDetails> cartDetails { get; set; }
+
         public int usersId { get; set; }
         public UserRegistration users { get; set; }
     }
