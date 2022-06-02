320130713e444965aa33ab9429bd02e633d6dbac
Akza <avarghese@suyati.com>
2022-06-02 18:38:30 +0530

    done

---
+++ UILayer1/wwwroot/js/searchBar.js
@@ -0,0 +1,29 @@
+﻿
+            var nameList = [];
+            $(document).ready(function () {
+                $("#filter").click(function () {
+                    $("#dropdown1").toggle();
+                });
+                $("#sort").click(function () {
+                    $("#dropdown2").toggle();
+                });
+                $.ajax({
+                    url: 'https://mobizoneapi.azurewebsites.net/api/productop/GetAll',
+                    dataType: 'Json',
+                    success: function (data) {
+                        console.log(data.result)
+                        var resultData = data.result;
+                        resultData.forEach(function (items, index) {
+                            console.log(items.name)
+                            nameList.push(items.name);
+                            console.log(nameList)
+                            $("#tags").autocomplete({
+                                source: nameList
+                            });
+                        });
+                    }
+                })
+
+            });
+
+
