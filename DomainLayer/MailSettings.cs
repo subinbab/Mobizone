ec6c13515b813e212fae3f3af15f1a6dd90ff919
Darwin <dcabral@suyati.com>
2022-06-21 19:12:25 +0530

    mail id changed

--- DomainLayer/MailSettings.cs
+++ DomainLayer/MailSettings.cs
@@ -8,9 +8,9 @@ namespace DomainLayer
     public class MailSettings
     {
 
-        public string Mail = "darwincabral9@gmail.com";
-        public string DisplayName = "Darwin Cabral";
-        public string Password = "ipkbvycsebplzrcm";
+        public string Mail = "mobizonesuyati@gmail.com";
+        public string DisplayName = "Mobizone";
+        public string Password = "wfsqhzxlibugwaja";
         public string Host = "smtp.gmail.com";
         public int Port = 587;
 
