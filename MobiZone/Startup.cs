e019813c2eb975e1b15b0de39317a62ba263b537
Akza <avarghese@suyati.com>
2022-06-28 13:14:16 +0530

    changes done

--- MobiZone/Startup.cs
+++ MobiZone/Startup.cs
@@ -4,28 +4,14 @@ using BusinessObjectLayer;
 using BusinessObjectLayer.User;
 using DomainLayer;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 using Repository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using System.Web.Helpers;
 
 namespace MobiZone
 {
