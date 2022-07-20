283ba6c081c846dd4b6f9806ab668bc6be99852b
Subin Babu <73779281+subinbab@users.noreply.github.com>
2022-07-20 18:27:02 +0530

    added invoice download facility

---
+++ UILayer1/Controllers/InvoiceController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UILayer.Data;
+using UILayer.Data.ApiServices;
+
+namespace UILayer.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        IConfiguration _configuration;
+        IUserApi _userApi;
+        private readonly IMapper _mapper;
+        public InvoiceController(IConfiguration configuration, IMapper mapper, IUserApi userApi)
+        {
+            _mapper = mapper;
+            _configuration = configuration;
+            _userApi = userApi;
+        }
+        public IActionResult Index(int id)
+        {
+            var checkoutList = _userApi.GetCheckOut().Result;
+            var checkout = checkoutList.Where(c => c.id.Equals(id)).FirstOrDefault();
+            adminApi _adminApi = new adminApi(_configuration, _mapper);
+            var contactData = _adminApi.ContactGet().Result.FirstOrDefault();
+            ViewBag.OrderData = checkout;
+            ViewBag.Contact = contactData;
+            return View();
+        }
+        [HttpPost]
+        [System.Web.Mvc.ValidateInput(false)]
+        public async Task<IActionResult> Download(string GridHtml,int id)
+        {
+            ConvertPdf _generatepdf = new ConvertPdf();
+            string destFile = _generatepdf.GenerateReportPDF(GridHtml, "invoice", "ClientLetterData");
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(destFile, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, "application/pdf", Path.GetFileName(destFile));
+        }
+    }
+}
---
+++ UILayer1/Data/ConvertPdf.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SelectPdf;
+namespace UILayer.Data
+{
+    public class ConvertPdf 
+    {
+        public string GenerateReportPDF(string content, string file, string reportname)
+        {
+            string fileName = "\\" + file + ".pdf";
+            string folderName = "\\" + DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss");
+            string relativePath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string folderPath = relativePath + "\\ReportsPDF" + "\\" + reportname + folderName;
+            string filePath = folderPath + fileName;
+            PdfPageSize pageSize = PdfPageSize.Letter;
+            PdfPageOrientation pdfOrientation = PdfPageOrientation.Landscape;
+            int webPageWidth = 1024;
+            int webPageHeight = 0;
+            // instantiate a html to pdf converter object
+            HtmlToPdf converter = new HtmlToPdf();
+            // set converter options
+            converter.Options.PdfPageSize = pageSize;
+            converter.Options.PdfPageOrientation = pdfOrientation;
+            converter.Options.WebPageWidth = webPageWidth;
+            converter.Options.WebPageHeight = webPageHeight;
+            converter.Options.MarginTop = 10;
+            converter.Options.MarginBottom = 10;
+
+            //create new folder inside PdfProcessing
+            System.IO.Directory.CreateDirectory(folderPath);
+
+            // create a new pdf document converting an url
+            PdfDocument doc = converter.ConvertHtmlString(content.ToString());
+
+            //doc.Security.CanEditContent = false;
+            //doc.Security.CanFillFormFields = false;
+
+            // save pdf document
+            byte[] pdfBytes = doc.Save();
+
+            // close pdf document
+            doc.Close();
+            FileStream fs = new FileStream(filePath, FileMode.Create);
+            fs.Write(pdfBytes, 0, pdfBytes.Length);
+            fs.Close();
+
+            return filePath;
+        }
+    }
+}
